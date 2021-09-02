07ce57f7cdb04e7207902713d421f2fba02a5f82
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2021-09-02 12:11:24 +0100

    Remove unwanted comments

--- Startup/main.c
+++ Startup/main.c
@@ -44,10 +44,6 @@
  
  *****************************************************************************/
 
-/*******************************************************************************
- * INCLUDES
- */
-
 #include <xdc/runtime/Error.h>
 
 #include <ti/sysbios/knl/Clock.h>
@@ -62,145 +58,57 @@
 #include "bcomdef.h"
 #ifdef PTM_MODE
 #include "npi_task.h"
-#endif // PTM_MODE
+#endif
 
-/* Header files required to enable instruction fetch cache */
 #include <inc/hw_memmap.h>
 #include <driverlib/vims.h>
 
 #ifndef USE_DEFAULT_USER_CFG
 #include "ble_user_config.h"
-// BLE user defined configuration
 icall_userCfg_t user0Cfg = BLE_USER_CFG;
-#endif // USE_DEFAULT_USER_CFG
+#endif
 
 #include <ti/display/Display.h>
-
-/*******************************************************************************
- * MACROS
- */
-
-/*******************************************************************************
- * CONSTANTS
- */
-
-/*******************************************************************************
- * TYPEDEFS
- */
-
-/*******************************************************************************
- * LOCAL VARIABLES
- */
-
-/*******************************************************************************
- * GLOBAL VARIABLES
- */
-
-/*******************************************************************************
- * EXTERNS
- */
 
 extern void AssertHandler(uint8 assertCause, uint8 assertSubcause);
 
 extern Display_Handle display;
 
-/*******************************************************************************
- * @fn          Main
- *
- * @brief       Application Main
- *
- * input parameters
- *
- * @param       None.
- *
- * output parameters
- *
- * @param       None.
- *
- * @return      None.
- */
 int main()
 {
-  /* Register Application callback to trap asserts raised in the Stack */
   RegisterAssertCback(AssertHandler);
 
   Board_initGeneral();
 
-  // Enable iCache prefetching
   VIMSConfigure(VIMS_BASE, TRUE, TRUE);
-  // Enable cache
   VIMSModeSet(VIMS_BASE, VIMS_MODE_ENABLED);
 
 #if !defined( POWER_SAVING )
-  /* Set constraints for Standby, powerdown and idle mode */
-  // PowerCC26XX_SB_DISALLOW may be redundant
   Power_setConstraint(PowerCC26XX_SB_DISALLOW);
   Power_setConstraint(PowerCC26XX_IDLE_PD_DISALLOW);
-#endif // POWER_SAVING
+#endif
 
-  /* Update User Configuration of the stack */
   user0Cfg.appServiceInfo->timerTickPeriod = Clock_tickPeriod;
   user0Cfg.appServiceInfo->timerMaxMillisecond  = ICall_getMaxMSecs();
 
-  /* Initialize ICall module */
   ICall_init();
 
-  /* Start tasks of external images - Priority 5 */
   ICall_createRemoteTasks();
 
 #ifdef PTM_MODE
-  /* Start task for NPI task */
   NPITask_createTask(ICALL_SERVICE_CLASS_BLE);
-#endif // PTM_MODE
+#endif
 
   simultaneous_sensor();
 
-  /* enable interrupts and start SYS/BIOS */
   BIOS_start();
 
   return 0;
 }
 
 
-/*******************************************************************************
- * @fn          AssertHandler
- *
- * @brief       This is the Application's callback handler for asserts raised
- *              in the stack.  When EXT_HAL_ASSERT is defined in the Stack Wrapper
- *              project this function will be called when an assert is raised,
- *              and can be used to observe or trap a violation from expected
- *              behavior.
- *
- *              As an example, for Heap allocation failures the Stack will raise
- *              HAL_ASSERT_CAUSE_OUT_OF_MEMORY as the assertCause and
- *              HAL_ASSERT_SUBCAUSE_NONE as the assertSubcause.  An application
- *              developer could trap any malloc failure on the stack by calling
- *              HAL_ASSERT_SPINLOCK under the matching case.
- *
- *              An application developer is encouraged to extend this function
- *              for use by their own application.  To do this, add hal_assert.c
- *              to your project workspace, the path to hal_assert.h (this can
- *              be found on the stack side). Asserts are raised by including
- *              hal_assert.h and using macro HAL_ASSERT(cause) to raise an
- *              assert with argument assertCause.  the assertSubcause may be
- *              optionally set by macro HAL_ASSERT_SET_SUBCAUSE(subCause) prior
- *              to asserting the cause it describes. More information is
- *              available in hal_assert.h.
- *
- * input parameters
- *
- * @param       assertCause    - Assert cause as defined in hal_assert.h.
- * @param       assertSubcause - Optional assert subcause (see hal_assert.h).
- *
- * output parameters
- *
- * @param       None.
- *
- * @return      None.
- */
 void AssertHandler(uint8 assertCause, uint8 assertSubcause)
 {
-  // Open the display if the app has not already done so
   if ( !display )
   {
     display = Display_open(Display_Type_ANY, NULL);
@@ -208,7 +116,6 @@ void AssertHandler(uint8 assertCause, uint8 assertSubcause)
 
   Display_print0(display, 0, 0, ">>>STACK ASSERT");
 
-  // check the assert cause
   switch (assertCause)
   {
     case HAL_ASSERT_CAUSE_OUT_OF_MEMORY:
@@ -217,7 +124,6 @@ void AssertHandler(uint8 assertCause, uint8 assertSubcause)
       break;
 
     case HAL_ASSERT_CAUSE_INTERNAL_ERROR:
-      // check the subcause
       if (assertSubcause == HAL_ASSERT_SUBCAUSE_FW_INERNAL_ERROR)
       {
         Display_print0(display, 0, 0, "***ERROR***");
@@ -257,26 +163,7 @@ void AssertHandler(uint8 assertCause, uint8 assertSubcause)
   return;
 }
 
-
-/*******************************************************************************
- * @fn          smallErrorHook
- *
- * @brief       Error handler to be hooked into TI-RTOS.
- *
- * input parameters
- *
- * @param       eb - Pointer to Error Block.
- *
- * output parameters
- *
- * @param       None.
- *
- * @return      None.
- */
 void smallErrorHook(Error_Block *eb)
 {
   for (;;);
 }
-
-/*******************************************************************************
- */
